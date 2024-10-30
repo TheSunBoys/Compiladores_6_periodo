6ec5526730758fb4e882d6049c1e075ccc94a704
Gabriel <batistutag190@gmail.com>
2024-10-30 17:06:28 -0300

    regex completa

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import re
 from colorama import Fore, Style
 
-regex = r'''x\[
+regex = r'''\[
     \s*
     (
-        -[1-9]+(:-[1-9]+)?
+        -[1-9]+(:-[1-9]+)?   # números negativos
         |
-        \d+(:\d+)?            # Inteiro positivo/negativo ou slice numérico
+        \d+(:\d+)?   # Inteiro positivo ou zero ou slice numérico com zero e números inteiros
         # |
-        # (['"][^\[\]"]*['"])         # String entre aspas simples ou duplas
+        # ['"]{1}[a-zA-Z ]+?['"]{1}:['"]{1}[a-zA-Z ]+?['"]{1}
+        |
+        '[a-zA-Z0-9 ]+?'(:'[a-zA-Z0-9 ]+?')?
+        |
+        "[a-zA-Z0-9 ]+?"(:"[a-zA-Z0-9 ]+?")?
+        # ['"]{1}[a-zA-Z ]+?['"]{1}
+        # (['"][^\[\]"]*['"])   # String entre aspas simples ou duplas
     )
     \s*
 \]
@@ -21,22 +27,24 @@ test_cases = [
     {"input": 'x[0:5]', "should_accept": True},
     {"input": 'x["column1"]', "should_accept": True},
     {"input": "x['column2']", "should_accept": True},
-    {"input": 'x["val1":"val2"]', "should_accept": False},
-    {"input": "x['val3':'val4']", "should_accept": False},
+    {"input": 'x["val1":"val2"]', "should_accept": True},
+    {"input": "x['val3':'val4']", "should_accept": True},
     {"input": 'x[-2:-1]', "should_accept": True},
     {"input": 'x[1:]', "should_accept": False},
-    {"input": 'x[1:-1]', "should_accept": True},
+    {"input": 'x[1:-1]', "should_accept": False},
     {"input": 'x["abc":0]', "should_accept": False},
     {"input": 'x[0:"abc"]', "should_accept": False},
     {"input": 'x["val1":1]', "should_accept": False},
     {"input": 'x[1:3]', "should_accept": True},
     {"input": 'x[-1:-3]', "should_accept": True},
-    {"input": 'x[-1:0]', "should_accept": True},
-    {"input": 'x["string1":"string2"]', "should_accept": False},
+    {"input": 'x[-1:0]', "should_accept": False},
+    {"input": 'x["string1":"string2"]', "should_accept": True},
     {"input": 'x[0:5.5]', "should_accept": False},
     {"input": 'x[]', "should_accept": False},
     {"input": 'x[:"end"]', "should_accept": False},
-    {"input": "x['end':'start']", "should_accept": False},
+    {"input": "x['end':'start']", "should_accept": True},
+    {"input": "x['end']", "should_accept": True},
+    {"input": "x['end ']", "should_accept": True},
 ]
 
 Trues = []
@@ -47,7 +55,7 @@ for test in test_cases:
     is_valid = match is not None
     should_accept = test["should_accept"]
 
-    if is_valid == should_accept:
+    if is_valid:
         print(test["input"])
         if match:
             print(f"{Fore.GREEN}Entrada: {test['input']} -> Capturado: {match.group(1)} (Válido){Style.RESET_ALL}")
